2193d0cf4d06db9b7d8f6c0a37d12dc1a6615ae6
dineshba <dineshb@INDINESHB.local>
2015-12-18 12:12:17 +0530

    Testing SnapCi

--- test/com/company/FirstAppTest.java
+++ test/com/company/FirstAppTest.java
@@ -2,6 +2,8 @@ package com.company;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class FirstAppTest {
@@ -10,5 +12,11 @@ public class FirstAppTest {
     public void firstAppMethod() {
         FirstApp firstApp = new FirstApp();
         assertTrue(firstApp.getValue());
+    }
+
+    @Test
+    public void shouldReturnTrue() {
+        FirstApp firstApp = new FirstApp();
+        assertThat(firstApp.getValue(), is(false));
     }
 }
